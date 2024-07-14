2f669a4066486fd0bfe5c20ceccf67b7b4da33d1
millenniumSonule <anshumansonuley@gmail.com>
2024-07-14 18:57:30 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import './App.css'
+import plp from './components/plp'
+import cart from './components/cart'
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
+
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter> 
+      <Routes>
+        <Route path='/' element = {<plp/>} />
+        <Route path ='/cart' element = {<cart/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/plp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const plp = () => {
+  return (
+    <div>plp</div>
+  )
+}
+
+export default plp
