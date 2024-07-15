5469c9d0f4225b587df3834a3d81a25884912379
millenniumSonule <anshumansonuley@gmail.com>
2024-07-15 13:19:38 +0530

    .

--- src/components/plp.jsx
+++ src/components/plp.jsx
@@ -6,17 +6,17 @@ const Plp = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null)
-    useEffect (() => {
+    useEffect(() => {
         fetchProducts();
-    },[]);
+    }, []);
 
-    const fetchProducts = async ()=> {
-        try{
+    const fetchProducts = async () => {
+        try {
             const responce_product = await axios.get('https://fakestoreapi.com/products');
-            setProducts(responce_product);
-        }catch(error){
+            setProducts(responce_product.data);
+        } catch (error) {
             setError(error);
-        }finally{
+        } finally {
             setLoading(false);
         }
     }
@@ -25,10 +25,15 @@ const Plp = () => {
     return (
         <div className="container">
             <div className="nav">
-                <Navbar/>
+                <Navbar />
             </div>
             <div className="input-box">
-                {JSON.stringify(products.data.price)}
+                {products.map((item, index) => (
+                    <div key={index} className="list-products">
+                        <p>{item.title}</p>
+                        <p>{item.category}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
