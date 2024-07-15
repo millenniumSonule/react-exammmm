82ab60b1b7506443ea8164a3f52bffa990065fad
millenniumSonule <anshumansonuley@gmail.com>
2024-07-15 12:48:22 +0530

    .

--- src/components/plp.jsx
+++ src/components/plp.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Navbar from './navbar'
-const plp = () => {
-  return (
+import axios from 'axios';
+
+const Plp = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
+    useEffect (() => {
+        fetchProducts();
+    },[]);
+
+    const fetchProducts = async ()=> {
+        try{
+            const responce_product = await axios.get('https://fakestoreapi.com/products');
+            setProducts(responce_product);
+        }catch(error){
+            setError(error);
+        }finally{
+            setLoading(false);
+        }
+    }
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error}</p>;
+    return (
         <div className="container">
             <div className="nav">
                 <Navbar/>
             </div>
             <div className="input-box">
-                
+                {JSON.stringify(products.data.price)}
             </div>
         </div>
     )
 }
 
-export default plp;
+export default Plp;
